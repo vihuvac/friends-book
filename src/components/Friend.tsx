ef78343893d0cff05f33b2c5d51afb81d111f116
Víctor Hugo Valle Castillo <victouk@gmail.com>
2024-06-09 11:01:36 -0400

    Added some styles for the friend component.

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -5,19 +5,39 @@ import type {Friend as FriendType} from '../redux/reducers/friend.reducer';
 
 export const Friend = (friend: FriendType) => {
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Image source={{uri: friend.picture.large}} style={styles.image} />
-      <View>
-        <Text>{`${friend.name.title} ${friend.name.first} ${friend.name.last}`}</Text>
-        <Text>{friend.email}</Text>
+      <View style={styles.content}>
+        <Text
+          style={
+            styles.friendName
+          }>{`${friend.name.title} ${friend.name.first} ${friend.name.last}`}</Text>
+        <Text style={styles.friendEmail}>{friend.email}</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
   image: {
     width: 50,
     height: 50,
+    marginRight: 16,
+  },
+  content: {
+    justifyContent: 'space-around',
+  },
+  friendName: {
+    fontSize: 16,
+  },
+  friendEmail: {
+    color: '#777',
   },
 });
